fb9e3e2a6c3404fb7a4f08ca9b59a76bda2619ff
thenaijagamer <sabdullahialaba50@gmail.com>
2022-12-24 17:27:07 +0100

    Refactor the header component

--- src/component/header/header.component.jsx
+++ src/component/header/header.component.jsx
@@ -1,19 +1,17 @@
 import React from "react";
-import { Link, useParams, useHref } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import HeaderMenuItem from "../header-menu-item/header-menu-item.component";
 
 import { ReactComponent as Logo } from "../../assets/shared/logo.svg";
 
 import "./header.styles.scss";
+import { useState } from "react";
 
 const Header = () => {
-  const handleUnchecked = (e) => {
-    const checkBox = e.target.previousSibling;
-
-    if (checkBox.checked) {
-      checkBox.checked = false;
-    }
+  const [hidden, setHidden] = useState(true);
+  const toggleHidden = () => {
+    setHidden(!hidden);
   };
   return (
     <div className="header">
@@ -21,15 +19,18 @@ const Header = () => {
         <Logo className="header__logo" />
       </Link>
       <div className="header__line"></div>
-      <input
-        type="checkbox"
-        className="navigation__checkbox"
-        id="navi-toggle"
-      />
-      <div className="overlay" onClick={handleUnchecked}></div>
-      <label htmlFor="navi-toggle" className="navigation__button">
-        <span className="navigation__icon">&nbsp;</span>
-      </label>
+      <div className="overlay"></div>
+      <div className="navigation__button" onClick={toggleHidden}>
+        <span
+          className={`navigation__icon ${hidden ? "" : "navigation__icon--1"}`}
+        ></span>
+        <span
+          className={`navigation__icon ${hidden ? "" : "navigation__icon--2"}`}
+        ></span>
+        <span
+          className={`navigation__icon ${hidden ? "" : "navigation__icon--3"}`}
+        ></span>
+      </div>
       <ul className="header__menu-container">
         <HeaderMenuItem num={"00"} name={"Home"} link={"/"} />
         <HeaderMenuItem
