6a1177424225613d5f1c887f8bce0b35fd20b5b7
thenaijagamer <sabdullahialaba50@gmail.com>
2022-12-25 15:57:21 +0100

    Refactor header component again

--- src/component/header/header.component.jsx
+++ src/component/header/header.component.jsx
@@ -19,27 +19,47 @@ const Header = () => {
         <Logo className="header__logo" />
       </Link>
       <div className="header__line"></div>
-      <div className="overlay"></div>
-      <div className="navigation__button" onClick={toggleHidden}>
-        <span
-          className={`navigation__icon ${hidden ? "" : "navigation__icon--1"}`}
-        ></span>
-        <span
-          className={`navigation__icon ${hidden ? "" : "navigation__icon--2"}`}
-        ></span>
-        <span
-          className={`navigation__icon ${hidden ? "" : "navigation__icon--3"}`}
-        ></span>
+      <div
+        className={`${hidden ? "" : "overlay"}`}
+        onClick={toggleHidden}
+      ></div>
+      <div
+        className={`nav__button ${hidden ? "" : "nav__button--cross"}`}
+        onClick={toggleHidden}
+      >
+        <span className={`nav__icon ${hidden ? "" : "nav__icon--1"}`}></span>
+        <span className={`nav__icon ${hidden ? "" : "nav__icon--2"}`}></span>
+        <span className={`nav__icon ${hidden ? "" : "nav__icon--3"}`}></span>
       </div>
-      <ul className="header__menu-container">
-        <HeaderMenuItem num={"00"} name={"Home"} link={"/"} />
+      <ul
+        className={`header__menu-container ${
+          hidden ? "header__menu-container--hide" : ""
+        }`}
+      >
+        <HeaderMenuItem
+          num={"00"}
+          name={"Home"}
+          link={"/"}
+          onClick={toggleHidden}
+        />
         <HeaderMenuItem
           num={"01"}
           name={"destination"}
           link={"/destination/Moon"}
+          onClick={toggleHidden}
         />
-        <HeaderMenuItem num={"02"} name={"Crews"} link={"/crew"} />
-        <HeaderMenuItem num={"03"} name={"technology"} link={"/technology/0"} />
+        <HeaderMenuItem
+          num={"02"}
+          name={"Crews"}
+          link={"/crew"}
+          onClick={toggleHidden}
+        />
+        <HeaderMenuItem
+          num={"03"}
+          name={"technology"}
+          link={"/technology/0"}
+          onClick={toggleHidden}
+        />
       </ul>
     </div>
   );
